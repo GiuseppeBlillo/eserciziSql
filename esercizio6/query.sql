aac2c20d4fe2579834ba1916ed67b03d6a97eeb1
GiuseppeBlillo <129520746+GiuseppeBlillo@users.noreply.github.com>
2023-06-15 11:46:11 +0200

    corretto es6

--- esercizio6/query.sql
+++ esercizio6/query.sql
@@ -1,10 +1,11 @@
 -- Using a SELECT statement with a GROUP BY clause and COUNT function, retrieve the number of orders for each meal.
-SELECT  MEAL_NAME, COUNT (*) AS NUMBER_OF_ORDERS FROM MEAL_ORDER_WITH_DETAILS
+SELECT  MEAL_NAME, COUNT (*) AS NUMBER_OF_ORDERS
+FROM MEAL_ORDER_WITH_DETAILS
 GROUP BY MEAL_NAME;
 
 -- Using a SELECT statement with a WHERE clause and SUM function, retrieve the total revenue for all meals that have a price greater than €15.
-SELECT MEAL_NAME,
-SUM(PRICE) AS TOTAL_REVENUE_OF_MEALS FROM MEAL_ORDER_WITH_DETAILS
+SELECT MEAL_NAME, SUM(PRICE) AS TOTAL_REVENUE_OF_MEALS 
+FROM MEAL_ORDER_WITH_DETAILS
 WHERE PRICE>15
 GROUP BY MEAL_NAME;
 
@@ -16,7 +17,7 @@ WHERE CALORIES>500;
 -- Using a SELECT statement with a WHERE clause and SUM function, retrieve the total price for all meals that have a name containing the letter 'S'.
 SELECT SUM(PRICE) AS TOTAL_PRICE_OF_MEALS_CONTAINING_S
 FROM MEAL
-WHERE NAME  LIKE '%s%';
+WHERE NAME LIKE '%s%';
 
 -- You can find items that start with the letter s with the following where clause WHERE LOWER(NAME) LIKE '%s%';
 SELECT NAME
