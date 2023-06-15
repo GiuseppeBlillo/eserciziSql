c37e98099bf650b3921db86c325dda6bb4cdee1e
GiuseppeBlillo <129520746+GiuseppeBlillo@users.noreply.github.com>
2023-06-15 11:48:42 +0200

    corretto es8

--- esercizio8/query.sql
+++ esercizio8/query.sql
@@ -1,10 +1,15 @@
 -- Add constraints to every column of the INGREDIENTS table, you should use NOT NULL for some columns and unique for other(s)
-alter table INGREDIENT add constraint unique (name);
+alter table INGREDIENT 
+add constraint unique (name);
 
-alter table INGREDIENT modify CONTAINS_LACTOSE boolean not null;
+alter table INGREDIENT 
+modify CONTAINS_LACTOSE boolean not null;
 
-alter table INGREDIENT modify VEGETARIAN boolean not null;
+alter table INGREDIENT 
+modify VEGETARIAN boolean not null;
 
-alter table INGREDIENT modify VEGAN boolean not null;
+alter table INGREDIENT
+modify VEGAN boolean not null;
 
-alter table INGREDIENT modify GLUTEN_FREE boolean not null;
+alter table INGREDIENT 
+modify GLUTEN_FREE boolean not null;
